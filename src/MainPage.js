84b74504d545af24061c8fb48e297e4df4f2cdab
Thien <nkt165@gmail.com>
2019-01-07 14:10:09 +0700

    add friend list

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import invo from './images/avatar/invo.jpg';
 
 const ChatBox = props => {
     return (
@@ -12,7 +11,7 @@ const ChatBox = props => {
                                 <div className="ui comments">
                                   <div className="comment">
                                     <a className="avatar">
-                                      <img src={invo}></img>
+                                      <img src="https://api.adorable.io/avatars/285/invoker"></img>
                                     </a>
                                     <div className="content">
                                       <a className="author">{messageItem.name}</a>
@@ -37,11 +36,56 @@ const ChatBox = props => {
     );
 }
 
+const FriendsList = props => {
+    return (
+      <div className="ui segment ">
+        <h3>Friends you know</h3>
+        <div className="ui middle aligned animated list">
+            {
+                props.knownFriends.map((friendItem, i) => {
+                    return (
+                        <div class="item">
+                          <div class="right floated content">
+                            {friendItem.isOnline && <div class="dot"></div>}
+                          </div>
+                          <img class="ui avatar image" src="https://api.adorable.io/avatars/285/asiojdioasd" />
+                          <div class="content">
+                            <a class="header">{friendItem.name}</a>
+                            <div class="description">{friendItem.pastMessage}</div>
+                          </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        <h3>People you may know</h3>
+        <div class="ui middle aligned list">
+            {
+                props.potentialFriends.map((friendItem, i) => {
+                    return (
+                        <div class="item">
+                          <div class="right floated content">
+                            <div class="ui button">Add</div>
+                          </div>
+                          <img class="ui avatar image" src="https://api.adorable.io/avatars/285/asiojdioasd" />
+                          <div class="content">
+                            <a class="header">{friendItem.name}</a>
+                          </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+      </div>
+    )
+}
+
 class MainPage extends Component {
     state = {
         messageData: [
             {
                 name: "Invoker",
+                avatar: "",
                 date: "123:123 AM",
                 message: "this is a test",
             },
@@ -52,13 +96,39 @@ class MainPage extends Component {
             },
         ],
         isTyping: true,
-        
+        knownFriends: [
+            {
+                name: "Windrunner",
+                avatar: "",
+                isOnline: true,
+                pastMessage: "A wind of change is blowing.",
+            },
+            {
+                name: "Crystal Maiden",
+                avatar: "",
+                isOnline: false,
+                pastMessage: "Who calls the Crystal Maiden?",
+            },
+        ],
+        potentialFriends: [
+            {
+                name: "Troll Warlord",
+                avatar: "",
+            },
+            {
+                name: "Huskar",
+                avatar: "",
+            },
+        ],   
     };
     render() {
         return (
             <div className="ui grid">
                 <div className="four wide column">
-                    <h1>Friends list</h1>
+                    <FriendsList
+                        knownFriends={this.state.knownFriends}
+                        potentialFriends={this.state.potentialFriends}
+                    />
                 </div>
                 <div className="six wide column">          
                     <ChatBox 
@@ -69,8 +139,7 @@ class MainPage extends Component {
                 <div className="six wide column">
                     <h1>News feed</h1>
                 </div>
-            </div>
-            
+            </div>   
         );
     }
 }
